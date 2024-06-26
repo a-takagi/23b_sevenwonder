3c7942bd816d889ba75fc0229deaf3e63cd9a90c
Ayuko Takagi <53387633+a-takagi@users.noreply.github.com>
2024-06-26 17:43:13 +0900

    高木GameManager途中

---
+++ 23b_sevenwonder/Assets/takagi/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    public void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 23b_sevenwonder/Assets/takagi/Scripts/SoundManager.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : SingletonMonoBehaviour<SoundManager>
+{
+
+    //BGMのファイル名　拡張子無し
+    //Assets/Resources/Sound/BGMの中に入れること
+    string[] BGMFileName = new string[]
+    {
+        "ed（仮）", //ゲームオーバー
+    };
+
+    //SEのファイル名　拡張子無し
+    //Assets/Resources/Sound/SEの中に入れること
+    string[] SEFileName = new string[]
+    {
+        "アナログパスワード入力_2", //ピ
+        "魔法、星", //入手
+        "鍵の開く音", //鍵
+        "携帯電話のバイブレーション1", //携帯
+        "部屋のドアを開く・閉める" //ガチャ
+    };
+
+    bool isFadeOut=false;
+    private float FadeOutSeconds = 1.0f;
+    float FadeDeltaTime = 0f;
+
+    private AudioSource audiosource;
+
+    //SE専用のAudioSource　FadeOutしないために
+    public GameObject SE;
+    private AudioSource seaudiosource;
+
+    public void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        seaudiosource = SE.GetComponent<AudioSource>();
+
+
+    }
+
+    void Update()
+    {
+        //Debug.Log("volume:" + audiosource.volume);
+        //Debug.Log("isFadeOut" + isFadeOut);
+
+        if (isFadeOut)
+        {
+            FadeDeltaTime += Time.deltaTime;
+            if(FadeDeltaTime >= FadeOutSeconds)
+            {
+                FadeDeltaTime = FadeOutSeconds;
+                isFadeOut = false;
+                BGMStop();
+            }
+            audiosource.volume = (float)(1.0f - FadeDeltaTime / FadeOutSeconds);
+        }
+        else
+        {
+            audiosource.volume = 1.0f;
+        }
+    }
+
+    public void StartBGM()
+    {
+        //シーン名に応じて再生する
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Title":
+                //SelectBGM("Title");
+                break;
+            case "TGS":
+                //SelectBGM("Title");
+                break;
+            case "GameOver":
+                SelectBGM("GameOver");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void BGMPlay()
+    {
+        audiosource.Play();
+    }
+
+    public void BGMStop()
+    {
+        audiosource.Stop();
+    }
+
+    public void FadeOutBgm()
+    {
+        Debug.Log("FadeOutBgm is called");
+        FadeDeltaTime = 0f;
+        isFadeOut = true;
+    }
+
+    /**
+    * SelectBGM
+    * 引数　string name ; BGM名
+    */
+    public void SelectBGM(string name)
+    {
+        int i;
+        Debug.Log("BGMPlay:" + name + " length:" + BGMFileName.Length);
+        if (audiosource == null)
+        {
+            audiosource = GetComponent<AudioSource>();
+        }
+        switch (name)
+        {
+            case "GameOver":
+                i = 0; //GameOver
+                break;
+            default:
+                i = 0;
+                break;
+        }
+        if (i >= BGMFileName.Length)
+        {
+            i = BGMFileName.Length - 1;
+        }
+        AudioClip tmp;
+        tmp = Resources.Load("Sound/BGM/" + BGMFileName[i]) as AudioClip;
+        if (!tmp)
+        {
+            Debug.Log("Sound/BGM/" + BGMFileName[i] + " is Missing");
+        }
+        else
+        {
+            audiosource.clip = tmp;
+        }
+        BGMPlay();
+    }
+
+    /**
+    * SEPlay
+    * 引数　string name ; SE名
+    */
+    public void SEPlay(string name)
+    {
+        int i;
+        Debug.Log("SEPlay:" + name + " length:" + SEFileName.Length);
+        if (seaudiosource == null)
+        {
+            seaudiosource = SE.GetComponent<AudioSource>();
+        }
+        switch (name)
+        {
+            case "ピ":
+                i = 0; //ピ
+                break;
+            case "入手":
+                i = 1; //入手
+                break;
+            case "鍵":
+                i = 2; //鍵
+                break;
+            case "携帯":
+                i = 3; //携帯
+                break;
+            case "ガチャ":
+                i = 4;//ガチャ
+                break;
+            default:
+                i = 0;
+                break;
+        }
+        if (i >= SEFileName.Length)
+        {
+            i = SEFileName.Length - 1;
+        }
+        Debug.Log("aa");
+        AudioClip tmp;
+        tmp = Resources.Load("Sound/SE/" + SEFileName[i]) as AudioClip;
+        if (!tmp)
+        {
+            Debug.Log(name + " is Missing");
+        }
+        seaudiosource.PlayOneShot(tmp);
+    }
+
+
+
+}
