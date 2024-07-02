4dc98f1b729eb931f7184dfd03a21831e5853fae
23b727 <23b727@shohoku.ac.jp>
2024-07-03 02:56:50 +0900

    Itemあたり判定
    高木先生を崇めよ

---
+++ 23b_sevenwonder/Assets/23b727/ItemSearch.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSearch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Debug.Log("Itemがボタンに触れる");
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //Fire1ボタンが押された
+                Debug.Log("Fire1が押された");
+            }
+        }
+    }
+}
+
