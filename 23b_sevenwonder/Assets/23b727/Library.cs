de6f7d8f6634e56805655356f8b24719479bb096
23b727 <23b727@shohoku.ac.jp>
2024-09-25 18:20:46 +0900

    図書室メッセージ
    ここには無さそうを3つ

--- 23b_sevenwonder/Assets/23b727/Library.cs
+++ 23b_sevenwonder/Assets/23b727/Library.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Library : MonoBehaviour
 {
+    [SerializeField]
+    GameObject kirakira1;
+
+    [SerializeField]
+    GameObject kirakira2;
+
+    [SerializeField]
+    GameObject kirakira3;
+
+    GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GameManager‚ÌŽæ“¾
+        GameObject tmp;
+        tmp = GameObject.Find("GameManager");
+        gm=tmp.GetComponent<GameManager>();
+        if (!gm)
+        {
+            Debug.Log("Door.cs: GameManager‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
