49d57067a16aeb05066be6d60b58cc385ef21c27
23b727 <23b727@shohoku.ac.jp>
2024-09-26 09:22:28 +0900

    職員室
    鍵途中

---
+++ 23b_sevenwonder/Assets/23b727/KirakiraS.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[RequireComponent(typeof(Flowchart))]
+public class KirakiraS : MonoBehaviour
+{
+    [SerializeField]  
+    string message = "";
+
+    [SerializeField]
+    GameObject kaiwa;
+    Flowchart flowchart;
+
+    GameManager gm;
+    
+    void OnTriggerStay2D(Collider2D col){
+    if(Input.GetButtonDown("Fire1")){
+        StartCoroutine(Talk());
+    }
+    }
+
+    IEnumerator Talk(){
+        flowchart.SendFungusMessage(message);
+        yield return new WaitUntil(() => flowchart.GetExecutingBlocks().Count == 0);
+        this.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tmp;
+        tmp = GameObject.Find("GameManager");
+        gm=tmp.GetComponent<GameManager>();
+        if (!gm)
+        {
+            Debug.Log("Door.cs: GameManager‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ");
+        }
+
+        flowchart=kaiwa.GetComponent<Flowchart>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
