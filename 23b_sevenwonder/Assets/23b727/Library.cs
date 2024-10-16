1b7f77d0de6cfabd6c3051c0e79093124428b4c8
23b727 <23b727@shohoku.ac.jp>
2024-10-16 18:56:32 +0900

    図書室の選択肢をやりたかった
    なんか消したはずの保健室の鍵の会話出てくるなんで

--- 23b_sevenwonder/Assets/23b727/Library.cs
+++ 23b_sevenwonder/Assets/23b727/Library.cs
@@ -16,11 +16,14 @@ public class Library : MonoBehaviour
     [SerializeField]
     GameObject tarohanaobj;
 
-     [SerializeField]
+    [SerializeField]
     GameObject tarohanakaiwa1obj;
 
-     [SerializeField]
+    [SerializeField]
     GameObject tarohanakaiwa2obj;
+
+    [SerializeField]
+    GameObject tarohanakaiwa3obj;
 
     GameManager gm;
 
@@ -29,47 +32,72 @@ public class Library : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 最初に全てのオブジェクトを非アクティブにする
         tarohanaobj.SetActive(false);
-        //GameManagerの取得
-        GameObject tmp;
+        tarohanakaiwa1obj.SetActive(false);
+        tarohanakaiwa2obj.SetActive(false);
+        tarohanakaiwa3obj.SetActive(false);
+
+        // GameManagerの取得
+        GameObject tmp; 
         tmp = GameObject.Find("GameManager");
-        gm=tmp.GetComponent<GameManager>();
+        gm = tmp.GetComponent<GameManager>();
         if (!gm)
         {
             Debug.Log("Library.cs: GameManagerが見つかりません");
         }
-        tarohanakaiwa2obj.SetActive(false);
 
-        //2回目かどうかの処理
-        if(gm.GetLibrarySecond()==true){
-            kiranum=3;
+        // 2回目以降の処理
+        if (gm.GetLibrarySecond() == true)
+        {
+            kiranum = 3;
             tarohanaobj.SetActive(true);
             kirakira1.SetActive(false);
             kirakira2.SetActive(false);
             kirakira3.SetActive(false);
-            TaroHana2Show();
-            tarohanakaiwa1obj.SetActive(false);
+            TaroHana3Show(); // 2回目以降は別の会話を表示
+        }
+        else
+        {
+            // 初回の会話処理
+            StartFirstConversation();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        // 必要に応じて更新処理をここに追加
     }
 
-    public void IncreaseKira(){
+    public void IncreaseKira()
+    {
         kiranum++;
-        if(kiranum>=3){
-        tarohanaobj.SetActive(true);
+        if (kiranum >= 3)
+        {
+            tarohanaobj.SetActive(true);
         }
         gm.SetLibrarySecond(true);
     }
 
-    public void TaroHana2Show(){
-        //2回目の会話を表示したい
+    // 初回の会話フロー
+    void StartFirstConversation()
+    {
+        tarohanakaiwa1obj.SetActive(true); // 最初の会話を表示
+        StartCoroutine(WaitAndShowTaroHana2()); // 会話終了後に次の会話を表示
+    }
 
-        this.tarohanakaiwa2obj.SetActive(true);
+    // tarohanakaiwa1の後にtarohanakaiwa2を表示するコルーチン
+    IEnumerator WaitAndShowTaroHana2()
+    {
+        yield return new WaitForSeconds(5); // 5秒待機（会話が終わるまでの時間）
+        tarohanakaiwa1obj.SetActive(false); // 最初の会話を非表示に
+        tarohanakaiwa2obj.SetActive(true);  // 次の会話を表示
+    }
+
+    // 2回目以降の会話
+    public void TaroHana3Show()
+    {
+        tarohanakaiwa3obj.SetActive(true); // 2回目以降の会話を表示
     }
 }
