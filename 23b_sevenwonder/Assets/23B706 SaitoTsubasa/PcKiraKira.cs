d478cca53d0502a6f757d1c071b1eaf976ab90ca
Saito197-shohoku <23B706@shohoku.ac.jp>
2024-10-16 19:07:45 +0900

    PC教室を作ってるけど途中上げ

---
+++ 23b_sevenwonder/Assets/23B706 SaitoTsubasa/PcKiraKira.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[RequireComponent(typeof(Flowchart))]
+public class PcKiraKira : MonoBehaviour
+{
+    [SerializeField]  
+    string message = "";
+
+    [SerializeField]
+    GameObject kaiwa;
+    Flowchart flowchart;
+
+    PcRoomManager Pm;
+    
+    void OnTriggerStay2D(Collider2D col){
+    if(Input.GetButtonDown("Fire1")){
+        StartCoroutine(Talk());
+    }
+    }
+
+    IEnumerator Talk(){
+        flowchart.SendFungusMessage(message);
+        yield return new WaitUntil(() => flowchart.GetExecutingBlocks().Count == 0);
+        this.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tmp;
+        tmp = GameObject.Find("PcRoomManager");
+        Pm=tmp.GetComponent<PcRoomManager>();
+        if (!Pm)
+        {
+            Debug.Log("Door.cs: LibraryManagerが見つかりませんでした");
+        }
+
+        flowchart=kaiwa.GetComponent<Flowchart>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
