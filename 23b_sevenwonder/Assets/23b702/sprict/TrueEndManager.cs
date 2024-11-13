5a6daa671701869d898ca38eef0b2cf2a0e40e7d
Saito197-shohoku <23B706@shohoku.ac.jp>
2024-11-13 14:09:33 +0900

    Endingのシーンを調整しました

--- 23b_sevenwonder/Assets/23b702/sprict/TrueEndManager.cs
+++ 23b_sevenwonder/Assets/23b702/sprict/TrueEndManager.cs
@@ -1,26 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TrueEndManager : MonoBehaviour
 {
+    [SerializeField] private bool EndingEnded;
+    [SerializeField] private GameObject Kaiwa1;
+    //参照系
+    private GameManager gm;
+    private GameObject GameManager;
+    private ItemManager Im;
+    private GameObject ItemManager;
+    private PlayerController Pm;
+    private GameObject Player;
 
-    [SerializeField] private GameObject kaiwa1;
-
-    [SerializeField] private GameObject kaiwa2;
     // Start is called before the first frame update
     void Start()
     {
+        if(GameObject.Find("GameManager") != null){
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        GameManager = GameObject.Find("GameManger");
+        Debug.Log("Get GameManager");
+        }else{Debug.Log("Not Found GameManager");}
         
+        if(GameObject.Find("ItemManger") != null){
+        Im = GameObject.Find("ItemManger").GetComponent<ItemManager>();
+        ItemManager = GameObject.Find("ItemManger");
+        Debug.Log("Get ItemManger");
+        }else{Debug.Log("Not Found ItemManger");}
+
+        if(GameObject.Find("Player")){
+        Pm = GameObject.Find("Player").GetComponent<PlayerController>();
+        Player = GameObject.Find("Player");
+        }else{Debug.Log("Not Found Player");}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1")){
+        if(Kaiwa1 == null){
+            Destroy(Player);
+            Destroy(ItemManager);
+            Destroy(GameManager);
+            SceneManager.LoadScene("Title");
+        }
+        }        
     }
-    public void End(){
-        kaiwa1.SetActive(false);
-        kaiwa2.SetActive(true);
+
+    //会話を始めた時用メソッド
+    public void KaiwaNau(){
+        gm.KaiwaNau();
+    }
+
+    //会話終わった時用メソッド
+    public void KaiwaOwatade(){
+        gm.KaiwaOwatade();
     }
 }
